12973e5202e1f7fc5b0a6b8fbe0277c3c8ebabad
Maria <mbastilla2007@gmail.com>
2024-01-25 09:26:07 -0500

    trabajo dia 1

--- dia#1/main.py
+++ dia#1/main.py
@@ -6,4 +6,39 @@
 print("Hello World!")
 
 # ----Datos primitivos ---- 
-## Desarrollado por : Maria Jose Bastilla Osorio - 1098668885
+#1. String
+texto = " Campus"
+print(texto)
+print(type ( texto))
+#2.Int
+numeroentero= 1
+print (numeroentero)
+
+#3. Double
+numerodecimallargo = 3.14167582737283
+print (numerodecimallargo)
+#4. Float
+numeroDecimal = 3.1
+print (numeroDecimal)
+#5. Boolean
+booleano = True
+print (booleano)
+# ---- Entradas por cuenat del usuario ----
+entradausuarionumero = int(input ("ingresa tu edad"))
+print ("tu edad  es: ", entradausuarionumero)
+# ---- ciclos ----
+# ciclo for
+for i in range (5,10,2): # for contador in range (desde, hasta. pasos):
+    print (i)
+#ciclo while
+booleanito= true 
+while booleanito== true:#while condicion_a_cumplir :
+print ("Sigo vivo")
+
+# ---- condicionales ----
+texto1 = "campus"
+if texto1== "campus" :
+    print ("soy campus")
+ else 
+    print ("No soy campus")
+ ## Desarrollado por : Maria Jose Bastilla Osorio - 1098668885
