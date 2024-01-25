c718d04040974e3fad13a5393f5b7acadf838838
Maria <mbastilla2007@gmail.com>
2024-01-25 08:50:17 -0500

    comandos

--- dia#1/main.py
+++ dia#1/main.py
@@ -1,1 +1,9 @@
-print("Hello World!") 
+## ---------------------------------
+## ---- Ejercicio 1 ----
+## ---------------------------------
+
+# Impresion en consola 
+print("Hello World!")
+
+# ----Datos primitivos ---- 
+## Desarrollado por : Maria Jose Bastilla Osorio - 1098668885
