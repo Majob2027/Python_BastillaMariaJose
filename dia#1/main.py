85082704b4f2a4aa905c26141adb09c4fd45c88f
Majob2027 <mbastilla2007@gmail.com>
2024-01-25 19:41:09 -0500

    tarea

--- dia#1/main.py
+++ dia#1/main.py
@@ -10,6 +10,8 @@ print("Hello World!")
 texto = " Campus"
 print(texto)
 print(type ( texto))
+
+
 #2.Int
 numeroentero= 1
 print (numeroentero)
@@ -17,19 +19,29 @@ print (numeroentero)
 #3. Double
 numerodecimallargo = 3.14167582737283
 print (numerodecimallargo)
+
+
 #4. Float
 numeroDecimal = 3.1
 print (numeroDecimal)
+
+
 #5. Boolean
 booleano = True
 print (booleano)
+
+
 # ---- Entradas por cuenat del usuario ----
 entradausuarionumero = int(input ("ingresa tu edad"))
 print ("tu edad  es: ", entradausuarionumero)
+
+
 # ---- ciclos ----
 # ciclo for
 for i in range (5,10,2): # for contador in range (desde, hasta. pasos):
     print (i)
+
+
 #ciclo while
 booleanito= true 
 while booleanito== true:#while condicion_a_cumplir :
@@ -41,4 +53,28 @@ if texto1== "campus" :
     print ("soy campus")
  else 
     print ("No soy campus")
+
+# ---- funciones ----
+
+# Funcion sin parametros y sin retorno
+def di_hola():
+    print ("Hola")
+
+# funcion con parametros y con retono
+def mi_funcion (num1,num2)
+print num1+num2
+mi_funcion(3,4)
+
+# Funcion con parametros pero sin retorno
+def mi_funcion2 ()
+
+
+
+# funcion sin parametros y con retorno 
+
+# ---- arreglos----
+list [2,3,4,5]}
+print ("list")
+
+
  ## Desarrollado por : Maria Jose Bastilla Osorio - 1098668885
