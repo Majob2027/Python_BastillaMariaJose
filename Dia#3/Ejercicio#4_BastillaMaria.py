8df807a8b2e4b8ca82936502461cf034a27d3635
Majob2027 <mbastilla2007@gmail.com>
2024-01-29 16:02:03 -0500

    si

---
+++ Dia#3/Ejercicio#4_BastillaMaria.py
@@ -0,0 +1,14 @@
+# Exercise#1
+# cashier
+numu=(int(input("integer money")))
+while numu<1 or numu>1000:
+ print ("please write another number")
+ numu=(int(input("integer money")))
+
+res1=numu/10
+
+
+for i in range ( numu )
+
+
+ 
