9345d97d3a708a8f32cb349bc6441b168740e4f2
Majob2027 <mbastilla2007@gmail.com>
2024-01-29 19:36:57 -0500

    trabajo

--- Dia#3/Ejercicio#4_BastillaMaria.py
+++ Dia#3/Ejercicio#4_BastillaMaria.py
@@ -1,14 +1,35 @@
 # Exercise#1
 # cashier
-numu=(int(input("integer money")))
-while numu<1 or numu>1000:
+num=(int(input("integer money")))
+money=num
+while num<1 or num>1000:
  print ("please write another number")
- numu=(int(input("integer money")))
-
-res1=numu/10
+ num=(int(input("integer money")))
 
 
-for i in range ( numu )
+count_10 = 0
+count_5 = 0
+count_1 = 0
+
+
+if money >= 10:
+    count_10 = money // 10
+    money %= 10
+if money >= 5:
+    count_5 = money // 5
+    money %= 5
+count_1 = money
+
+# Calculate the total minimum number of coins required
+min_coins = count_10 + count_5 + count_1
+
+# Print the result
+print("Minimum number of coins:", min_coins)
+print("Number of 10 coins:", count_10)
+print("Number of 5 coins:", count_5)
+print("Number of 1 coins:", count_1)
+
+
 
 
  
