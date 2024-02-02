5fe98ce62d6ec6e2f004cfb358e498c494773a6e
Maria <mbastilla2007@gmail.com>
2024-02-02 09:07:15 -0500

    kk

--- Dia #7/Ejercicio1dia1_BastillaMaria.py
+++ Dia #7/Ejercicio1dia1_BastillaMaria.py
@@ -1,58 +1,57 @@
 ## ---------------------------------
 ## ---- Ejercicio 1 ----
 ## ---------------------------------
-def d():
  # Impresion en consola 
- print("Hello World!")
+print("Hello World!")
 
  # ----Datos primitivos ---- 
  #1. String
- texto = " Campus"
- print(texto)
- print(type ( texto))
+texto = " Campus"
+print(texto)
+print(type ( texto))
 
 
  #2.Int
- numeroentero= 1
- print (numeroentero)
+numeroentero= 1
+print (numeroentero)
 
  #3. Double
- numerodecimallargo = 3.14167582737283
- print (numerodecimallargo)
+numerodecimallargo = 3.14167582737283
+print (numerodecimallargo)
 
 
  #4. Float
- numeroDecimal = 3.1
- print (numeroDecimal)
+numeroDecimal = 3.1
+print (numeroDecimal)
 
 
  #5. Boolean
- booleano = True
- print (booleano)
+booleano = True
+print (booleano)
 
 
  # ---- Entradas por cuenat del usuario ----
- entradausuarionumero = int(input ("ingresa tu edad"))
- print ("tu edad  es: ", entradausuarionumero)
+entradausuarionumero = int(input ("ingresa tu edad"))
+print ("tu edad  es: ", entradausuarionumero)
 
 
  # ---- ciclos ----
  # ciclo for
- for i in range (5,10,2): # for contador in range (desde, hasta. pasos):
+for i in range (5,10,2): # for contador in range (desde, hasta. pasos):
     print (i)
 
 
  #ciclo while
- booleanito= True 
- while booleanito== True:#while condicion_a_cumplir :
-    print ("Sigo vivo")
- booleanito == False
+#booleanito= True 
+#while booleanito== False:#while condicion_a_cumplir :
+   # print ("Sigo vivo")
+#booleanito == False
 
  # ---- condicionales ----
- texto1 = "campus"
- if texto1== "campus" :
+texto1 = "campus"
+if texto1== "campus" :
     print ("soy campus")
- else:
+else:
     print ("No soy campus")
     
 
