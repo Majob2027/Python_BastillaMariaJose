87c2499c0dcc3fa398c53fd49d52506eb52002eb
Maria <mbastilla2007@gmail.com>
2024-02-08 08:06:04 -0500

    11

--- Dia8/Ejercicio10_BastillaMariaJose.py
+++ Dia8/Ejercicio10_BastillaMariaJose.py
@@ -7,5 +7,5 @@ listacomerciales=miJson['ventas']['comerciales']
 
 #10. Devuelve un listado con los nombres de los comerciales que tienen como apellido "Ruiz". Tenga en cuenta que se deberán eliminar los nombres repetidos.
 for i in listacomerciales:
-     if i ['apellido'] . startswith ("Ruiz"):
-          print(f"")
+     if i ['apellido1'] . startswith ("Ruiz"):
+          print(f"id: {i ['id']} nombre:{i['nombre']} Apellido1:{i['apellido1']} Apellido2:{i['apellido2']}Comision:{i['comision']}")
--- Dia8/Ejercicio2_BastillaMariaJose.py
+++ Dia8/Ejercicio2_BastillaMariaJose.py
@@ -9,7 +9,7 @@ listapedidos=mijson['ventas']['pedidos']
 pedidos_ordenados=sorted(listapedidos,  key=lambda p: p["total"],reverse=True )
 pedidos_mayores= pedidos_ordenados[:2]
 
-print("loa dos pedidos de mayor valor son:")
+print("los dos pedidos de mayor valor son:")
 for pedido in pedidos_mayores:
     print(f"Total: {pedido ['total']} id_cliente:{pedido['id_cliente']}")
 
