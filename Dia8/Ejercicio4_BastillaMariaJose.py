c850774f925f3759caa85a8d4b1bfb436ed7a8b3
Maria <mbastilla2007@gmail.com>
2024-02-08 07:44:24 -0500

    ..

--- Dia8/Ejercicio4_BastillaMariaJose.py
+++ Dia8/Ejercicio4_BastillaMariaJose.py
@@ -1,13 +1,16 @@
 import json
 
-x=open('data.json')
-miJson=json.load(x)
-listapedidos=miJson['ventas']['pedidos']
+x = open('data.json')
+miJson = json.load(x)
+listapedidos = miJson['ventas']['pedidos']
 
-# 4. Devuelve un listado de todos los pedidos que se realizaron durante el año 2017, cuya cantidad total sea superior a 500€.
-pe=[]
-for i in listapedidos :
-       if i['fecha'].startswith("2017") and ['ventas']['pedidos']['total']>500:
-             pe.append(i['total'])
+# 4. Devuelve un listado de todos los pedidos que se realizaron durante el año 2017,
+# cuya cantidad total sea superior a 500€.
+pe = []
+for pedido in listapedidos:
+    if pedido['fecha'].startswith("2017") and pedido['total'] > 500:
+        pe.append(pedido)
 
-print(pe)
+# Print the list of orders meeting the criteria
+for p in pe:
+    print(p)
