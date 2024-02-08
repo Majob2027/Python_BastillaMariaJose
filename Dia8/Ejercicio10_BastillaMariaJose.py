b7376cc42497a926922865a4142869b80f701acf
Maria <mbastilla2007@gmail.com>
2024-02-08 08:13:55 -0500

    ..

--- Dia8/Ejercicio10_BastillaMariaJose.py
+++ Dia8/Ejercicio10_BastillaMariaJose.py
@@ -8,4 +8,5 @@ listacomerciales=miJson['ventas']['comerciales']
 #10. Devuelve un listado con los nombres de los comerciales que tienen como apellido "Ruiz". Tenga en cuenta que se deberán eliminar los nombres repetidos.
 for i in listacomerciales:
      if i ['apellido1'] . startswith ("Ruiz"):
-          print(f"id: {i ['id']} nombre:{i['nombre']} Apellido1:{i['apellido1']} Apellido2:{i['apellido2']}Comision:{i['comision']}")
+      print(f"id: {i ['id']} nombre :{i['nombre']}")
+
